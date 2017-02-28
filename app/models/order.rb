ad54bc9a2c8a6db2c91045d0558322bd700fae26
Elliott Potter <elliott@shipt.com>
2017-02-28 16:08:52 +0100

    Creates customer or driver

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :customer
-  belongs_to :driver
+  belongs_to :driver, optional: true
+  has_many :order_products
   has_many :products, through: :order_products
 end
