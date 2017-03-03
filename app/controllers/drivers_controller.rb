5c6ec9b898fca06d7f7fb6418600c7a7e3919fe6
Elliott Potter <elliott@shipt.com>
2017-03-03 12:14:20 +0100

    Claim orders screen is up and running

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,14 +1,16 @@
 class DriversController < ApplicationController
 
   def show
-    @driver = current_user.driver
-    @orders = @driver.orders
+    @driver             = current_user.driver
+    @orders             = @driver.orders
+    @available_orders   = Order.unclaimed
+    @hello              = "Hello!"
     authorize @driver
   end
 
   def switch
-    @driver = current_user.driver
-    @driver.status == "active" ? @driver.deactivate! : @driver.activate!
+    @driver             = current_user.driver
+    @driver.status      == "active" ? @driver.deactivate! : @driver.activate!
     redirect_to driver_path(@driver)
     authorize @driver
   end
--- app/models/order.rb
+++ app/models/order.rb
@@ -4,31 +4,37 @@ class Order < ApplicationRecord
   has_many :order_products, inverse_of: :order
   has_many :products, through: :order_products
 
+
+
   accepts_nested_attributes_for :order_products
 
   include AASM
 
-  aasm :column => :status do
-    state :unclaimed, :initial => true
+  aasm do
+    state :unclaimed, initial: true
     state :claimed
     state :shopping
     state :processed
     state :delivered
 
+    event :unclaim do
+      transitions from: [ :claimed, :shopping, :processed, :delivered, :unclaimed ], to: :unclaimed
+    end
+
     event :claim do
-      transitions :from => :unclaimed, :to => :claimed
+      transitions from: :unclaimed, to: :claimed
     end
 
     event :shop do
-      transitions :from => :claimed, :to => :shopping
+      transitions from: :claimed, to: :shopping
     end
 
     event :process do
-      transitions :from => :shopping, :to => :processed
+      transitions from: :shopping, to: :processed
     end
 
     event :deliver do
-      transitions :from => :processed, :to => :delivered
+      transitions from: :processed, to: :delivered
     end
 
   end
