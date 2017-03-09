e58aa61be6e993c4c8d9bceaf9506bcaca2d6dc8
Soren Jensen <sorenkbj@gmail.com>
2017-03-09 15:55:47 +0100

    various namechanges

--- db/schema.rb
+++ db/schema.rb
@@ -69,15 +69,15 @@ ActiveRecord::Schema.define(version: 20170309131823) do
   end
 
   create_table "orders", force: :cascade do |t|
-    t.string   "aasm_state",                            default: "unpaid"
+    t.string   "aasm_state",    default: "unpaid"
     t.date     "delivery_date"
     t.datetime "delivered_at"
-    t.decimal  "subtotal",      precision: 8, scale: 2
+    t.decimal  "subtotal"
     t.integer  "customer_id"
     t.integer  "driver_id"
-    t.datetime "created_at",                                               null: false
-    t.datetime "updated_at",                                               null: false
-    t.integer  "amount_cents",                          default: 0,        null: false
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
+    t.integer  "amount_cents",  default: 0,        null: false
     t.json     "payment"
     t.index ["customer_id"], name: "index_orders_on_customer_id", using: :btree
     t.index ["driver_id"], name: "index_orders_on_driver_id", using: :btree
@@ -87,7 +87,6 @@ ActiveRecord::Schema.define(version: 20170309131823) do
     t.string   "name"
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
-    t.string   "photo_url"
     t.string   "url"
     t.integer  "price_cents", default: 0, null: false
   end
@@ -117,15 +116,6 @@ ActiveRecord::Schema.define(version: 20170309131823) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
-  create_table "verification_codes", force: :cascade do |t|
-    t.integer  "user_id"
-    t.string   "code"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string   "user_code"
-    t.index ["user_id"], name: "index_verification_codes_on_user_id", using: :btree
-  end
-
   add_foreign_key "customer_addresses", "customers"
   add_foreign_key "customers", "users"
   add_foreign_key "drivers", "users"
@@ -133,5 +123,4 @@ ActiveRecord::Schema.define(version: 20170309131823) do
   add_foreign_key "order_products", "products"
   add_foreign_key "orders", "customers"
   add_foreign_key "orders", "drivers"
-  add_foreign_key "verification_codes", "users"
 end
