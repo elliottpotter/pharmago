3bbc713e7fba2b9ede8171952b8a32b9d623cf1b
Soren Jensen <sorenkbj@gmail.com>
2017-03-10 09:31:56 +0100

    guys guys i fixed stripe

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -9,7 +9,7 @@ class PaymentsController < ApplicationController
 
   charge = Stripe::Charge.create(
     customer:     customer.id,   # You should store this customer id and re-use it.
-    amount:       @order.amount,
+    amount:       @order.amount_cents,
     description:  "Payment for order #{@order.id}",
     currency:     @order.amount.currency
   )
